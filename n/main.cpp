3132aacfe53a0154ddc1844099f7e14ec523a4ed
田昊冬 <xgpsthd0902@outlook.com>
2022-04-27 18:15:50 +0800

    Add file io

--- n/main.cpp
+++ n/main.cpp
@@ -1,3 +1,16 @@
-//
-// Created by tth37 on 2022/4/27.
-//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+#ifdef LOCAL_DEBUG
+  std::string path = PROJECT_SRC_DIR;
+  path += "/a";
+  freopen((path + "/test.in").c_str(), "r", stdin);
+  freopen((path + "/test.out").c_str(), "w", stdout);
+#endif
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  return 0;
+}
